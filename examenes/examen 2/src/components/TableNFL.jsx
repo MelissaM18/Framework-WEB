a59494c3dd14e4b6dbf21aa61b1e2b7ed5958b39
MelissaM18 <149817049+MelissaM18@users.noreply.github.com>
2024-03-14 22:21:10 -0700

    Add files via upload

---
+++ examenes/examen 2/src/components/TableNFL.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useTable } from 'react-table';
+
+const TableNFL = ({ columns, data }) => {
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({ columns, data });
+
+    return (
+        <div>
+            <div className="boton">
+                <a href="/"><button>Back</button></a>
+            </div>
+            <div className="tabla-contenido">
+                <table {...getTableProps()} className="tablaNFL">
+                    <thead>
+                        {headerGroups.map(headerGroup => (
+                            <tr {...headerGroup.getHeaderGroupProps()}>
+                                {headerGroup.headers.map(column => (
+                                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                                ))}
+                            </tr>
+                        ))}
+                    </thead>
+                    <tbody {...getTableBodyProps()}>
+                        {rows.map(row => {
+                            prepareRow(row);
+                            return (
+                                <tr {...row.getRowProps()}>
+                                    {row.cells.map(cell => {
+                                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                                    })}
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default TableNFL;
